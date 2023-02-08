c72d396dd6effa4bb4d2a45b92a4151995299a36
clement-papia <clement.papia@laplateforme.io>
2023-02-08 09:33:11 +0100

    Rendu convertisseur de monnaie
    ajout de l'interface graphique + modification pour que le convertisseur fonctionne !

--- convertisseur.py
+++ convertisseur.py
@@ -1,16 +1,33 @@
-import requests
+import tkinter as tk 
+import requests  
 
-def convertir_devise(montant, dev_initiale, dev_finale):
-    url = "https://exchangeratesapi.io" + dev_initiale
-    donnees = requests.get(url).json()
-    taux_change = donnees['rates'][dev_finale]
-    conversion = montant * taux_change
-    return conversion
 
-montant = float(input('Entrez le montant à convertir: '))
-dev_initiale = input('Entrez la devise initiale: ')
-dev_finale = input('Entrez la devise finale: ')
+def converti():
+    url = "https://api.exchangerate-api.com/v4/latest/EUR"
+    response = requests.get(url)
+    data = response.json()
+    exchange = data["rates"]["USD"]
 
-resultat = convertir_devise(montant, dev_initiale, dev_finale)
+    original = float(amount_entry.get())
+    converted = original * exchange
 
-print(str(montant) + ' ' + dev_initiale + ' = ' + str(resultat) + ' ' + dev_finale)
+    result_label.config(text=f"{converted:.2f} USD")
+    
+    test = open("test.txt","a")
+    test.write(f"{converted:.2f} USD\n")
+    test.close()
+
+windows = tk.Tk()
+windows.title("Convertisseur euro/dollar")
+windows.geometry('320x420')
+
+amount_entry = tk.Entry(windows)
+amount_entry.pack()
+
+converti_button = tk.Button(windows, text="Convertir", command=converti)
+converti_button.pack()
+
+result_label = tk.Label(windows, text="")
+result_label.pack()
+
+windows.mainloop()
